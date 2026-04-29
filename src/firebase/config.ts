993a105f43e85c2e8e6ed89e92624a933f432cb0
Araafath S <araafaths309@gmail.com>
2026-04-29 09:18:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4183448247-3e2b0",
+  "appId": "1:809684514588:web:dbac9a1a4ab9376f036cd9",
+  "apiKey": "AIzaSyCHjhDNSPe0nJsI1CA2_UedRL6JZNG-Js4",
+  "authDomain": "studio-4183448247-3e2b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "809684514588"
+};
